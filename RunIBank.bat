33536db81b2e06271003b97537fe7ec59d53261c
winitsky <winitsky@mail.ru>
2015-08-18 12:01:46 +0300

    fix bat file

--- RunIBank.bat
+++ RunIBank.bat
@@ -6,7 +6,6 @@ git clone -b develop https://github.com/winitsky/JavaEE.git
 cd /d d:\ibank\JavaEE\
 call mvn install
 
-mysql.exe -u root -p < D:\ibank\JavaEE\internetbank.dao\banking.sql
 
-cd internetbank.dao\target\
-java -jar internetbank.dao-0.0.1-SNAPSHOT.jar \dao\impl.RunDAO
+cd /d d:\ibank\JavaEE\
+call mvn tomcat7:redeploy
